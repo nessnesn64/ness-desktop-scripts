3a8cc4f1628859864151c0dbe11a07b3e1137058
Ness <baileywalters21@gmail.com>
2025-05-12 19:32:45 -0400

    Add Clipboard Verification

--- kde-screenshot-improvements/spectacle.sh
+++ kde-screenshot-improvements/spectacle.sh
@@ -13,16 +13,20 @@ then
 	echo "You are missing wl-clipboard. Please install this before using this script"
 fi
 
+#Clear the clipboard, to validate we're saving an image
+wl-copy --clear
+
+#Take a screenshot with Spectacle in Region mode, to clipboard, supressing notifs (which hangs spectacle otherwise)
+spectacle -bcrsn
+
 #Get relevant information of the window below the mouse cursor.
 windowid=$(kdotool getmouselocation --shell | grep WINDOW)
 windowid=${windowid//"WINDOW="/}
 
-echo $windowid
+#echo $windowid
 
+#Get pid of process for filename
 windowpid=$(kdotool getwindowpid $windowid)
-
-#echo $windowclass
-#echo $windowname
 echo $windowpid
 windowname=$(ps -p $windowpid -o comm=)
 echo $windowname
@@ -32,7 +36,7 @@ windowname=${windowname// /}
 
 #Strip .exe from the process name if there
 windowname=${windowname//".exe"/}
-echo $windowname
+#echo $windowname
 
 #Check if the directory structure already exists. If not, create it
 if [ ! -d "$HOME/Pictures/Screenshots/$(date +%Y)/$(date +%B)/" ];
@@ -48,14 +52,15 @@ then
 	mkdir "$HOME/Pictures/Screenshots/$(date +%Y)/$(date +%B)/"
 fi
 
-#Take a screenshot with Spectacle in Region mode
-spectacle -bcrs
-
-#Output clipboard to Screenshots for archiving
-wl-paste > $HOME/Pictures/Screenshots/$(date +%Y)/$(date +%B)/$(date '+%Y-%m-%d-%T').png
-
-
-
+#Test if clipboard contains image. Exit without saving if not.
+if wl-paste -l | grep -q 'image';
+then
+	#echo "picture found"
+	#Output clipboard to Screenshots for archiving
+	wl-paste > $HOME/Pictures/Screenshots/$(date +%Y)/$(date +%B)/$windowname-$(date '+%Y-%m-%d-%T').png
+else
+	exit
+fi
 
 
 
